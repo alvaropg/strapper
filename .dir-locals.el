94a0b6e7b38c198ad1343011e699305ba152c75f
Álvaro Peña <alvaropg@gmail.com>
2017-04-25 20:29:32 +0200

    Updated emacs clang flycheck config

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,6 @@
 ((c-mode
   (flycheck-clang-include-path "/usr/include/gstreamer-1.0"
                                "/usr/include/glib-2.0"
-                               "/usr/lib64/glib-2.0/include")))
+                               "/usr/lib64/glib-2.0/include"
+                               "/home/alvaropg/work/git/strapper/"
+                               "/home/alvaropg/work/git/strapper/src")))
